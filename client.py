e8f9c322ac2f9f2845df6282283705a6de349ba0
Kale Kundert <kale@thekunderts.net>
2012-12-19 19:54:44 -0800

    Remove the name argument from the client script.

--- client.py
+++ client.py
@@ -4,12 +4,11 @@ import argparse
 import slimycat
 
 parser = argparse.ArgumentParser()
-parser.add_argument('name')
 parser.add_argument('--host', '-x', default='localhost')
 parser.add_argument('--port', '-p', default=53351, type=int)
 
 arguments = parser.parse_args()
-name, host, port = arguments.name, arguments.host, arguments.port
+host, port = arguments.host, arguments.port
 
-game = slimycat.ClientLoop(name, host, port)
+game = slimycat.ClientLoop('Client', host, port)
 game.play()
