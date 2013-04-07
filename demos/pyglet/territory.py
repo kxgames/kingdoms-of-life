aaaae3a40b9519040994cab57d58b51f89b0c4dc
Kale Kundert <kale@thekunderts.net>
2013-04-07 10:50:48 -0700

    Expand the territory demonstration.

--- demos/pyglet/territory.py
+++ demos/pyglet/territory.py
@@ -3,28 +3,34 @@
 import pyglet
 import math
 
+def draw_donut(cx, cy, r1, r2, n=100):
+    points = ()
+
+    for i in range(n+2):
+        r = r1 if i % 2 else r2
+        x = cx + r * math.cos(2 * math.pi * i / n)
+        y = cy + r * math.sin(2 * math.pi * i / n)
+        points += (x, y)
+
+    points = points[0:2] + points + points[-2:]
+    return batch.add(n+4, pyglet.gl.GL_TRIANGLE_STRIP, None, ('v2f', points))
+
+
 window = pyglet.window.Window()
 batch = pyglet.graphics.Batch()
 
-points = ()
-n = 100
-cx, cy = 320, 240
-r1, r2 = 220, 150
-
-for i in range(n+2):
-    r = r1 if i % 2 else r2
-    x = cx + r * math.cos(2 * math.pi * i / n)
-    y = cy + r * math.sin(2 * math.pi * i / n)
-    points += (x, y)
-
-
-vertices = batch.add(n+2, pyglet.gl.GL_TRIANGLE_STRIP, None,
-    ('v2f', points),
-)
+donuts = (
+        draw_donut(170, 240, 50, 30),
+        draw_donut(320, 240, 50, 30),
+        draw_donut(470, 240, 50, 30) )
 
 @window.event
 def on_draw():
     window.clear()
     batch.draw()
+
+@window.event
+def on_key_release(*args):
+    donuts[1].delete()
 
 pyglet.app.run()
