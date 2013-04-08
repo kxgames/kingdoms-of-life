7090f1166d8b8a69eff4783c3c51a50bcff8df21
Kale Kundert <kale@thekunderts.net>
2013-04-07 23:10:46 -0700

    Add artwork to highlight movement commands.

--- images/selection.py
+++ images/selection.py
@@ -6,6 +6,11 @@ sys.path.append('..')
 import kxg
 import random
 import string
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--skip', '-k', action='store_true')
+arguments = parser.parse_args()
 
 vertices = []
 iterations = 36
@@ -15,6 +20,9 @@ position = 0, 0
 print '<svg xmlns="http://www.w3.org/2000/svg" version="1.1">'
 
 for x in range(iterations):
+    if arguments.skip and x % 2:
+        continue
+
     offset = kxg.geometry.Vector.from_degrees(x * 360 / iterations)
     vector = position + (radius * offset)
     print '<circle cx="%f" cy="%f" r="1.5"/>' % vector.tuple
