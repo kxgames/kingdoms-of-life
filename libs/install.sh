7d6eeed6ff8b7bf209a432d96523cfc35c68c5e9
Kale Kundert <kale@thekunderts.net>
2014-08-24 10:08:46 -0700

    Have install.sh complain about no-op arguments.

--- libs/install.sh
+++ libs/install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+if [ $# -eq 0 ]; then
+    echo "Usage: install.sh <libraries>..."
+    exit 1
+fi
+
 for distribution in $@; do
     pip install --editable $distribution
 done
